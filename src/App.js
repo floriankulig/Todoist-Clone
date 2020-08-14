daeaa852f3d1be9723824607643fc48d9ae3a85f
Florian Kulig <florian.kulig@web.de>
2020-08-14 18:27:49 +0200

    integrated darkMode saving into localStorage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
 
-export const App = ({ darkModeDefault = false }) => {
-  const [darkMode, setDarkMode] = useState(darkModeDefault);
+export const App = () => {
+  const getSavedMode = () => {
+    const savedMode = JSON.parse(localStorage.getItem("darkMode"));
+    if ("darkMode" in localStorage) {
+      return savedMode;
+    } else {
+      return false;
+    }
+  };
+
+  const [darkMode, setDarkMode] = useState(getSavedMode());
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", JSON.stringify(darkMode));
+  }, [darkMode]);
 
   return (
     <SelectedProjectProvider>
