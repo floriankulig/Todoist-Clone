79a649967e2e870acfc904687680185a6e5a1aee
Florian Kulig <florian.kulig@web.de>
2020-08-31 22:09:01 +0200

    added unsubscribe for efficient auth listening

--- src/context/user-context.js
+++ src/context/user-context.js
@@ -1,24 +1,27 @@
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { firebase } from "../firebase";
 
-export const UserContext = createContext({ user: null });
+export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((userAuth) => {
-      if (user) {
-        setUser(userAuth);
+    const unsubscribe = firebase.auth().onAuthStateChanged((authUser) => {
+      if (authUser) {
+        setUser(authUser);
+        console.log(user);
       } else {
+        setUser(null);
       }
-      console.log(user);
     });
-  });
 
-  return (
-    <UserContext.Provider value={{ user }}>{children}</UserContext.Provider>
-  );
+    return () => {
+      unsubscribe();
+    };
+  }, [user]);
+
+  return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
 };
 
 export const useAuthValue = () => useContext(UserContext);
