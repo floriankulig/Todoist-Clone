30f3f870e58580ee25f04b56ac7975d654dcbb97
Florian Kulig <florian.kulig@web.de>
2020-09-06 23:38:36 +0200

    added logic for when no user is signed in

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -4,12 +4,18 @@ import { AddTask } from "./AddTask";
 import { useTasks } from "../hooks";
 import { collatedTasks } from "../constants";
 import { getTitle, getCollatedTitle, collatedTasksExist } from "../helpers";
-import { useSelectedProjectValue, useProjectsValue } from "../context";
+import {
+  useSelectedProjectValue,
+  useProjectsValue,
+  useAuthValue,
+} from "../context";
+import { ReactComponent as Checklist } from "./assets/checklist.svg";
 
 export const Tasks = () => {
   const { selectedProject } = useSelectedProjectValue();
   const { projects } = useProjectsValue();
   const { tasks } = useTasks(selectedProject);
+  const currentUser = useAuthValue();
 
   let projectName = "";
 
@@ -27,23 +33,39 @@ export const Tasks = () => {
   }
 
   useEffect(() => {
-    document.title = `${projectName}: Todoist`;
+    currentUser.uid
+      ? (document.title = `${projectName}: Todoist`)
+      : (document.title = "Todoist");
   });
 
   return (
     <div className="tasks" data-testid="tasks">
-      <h2 data-testid="project-name">{projectName}</h2>
+      {currentUser.uid ? (
+        <h2 data-testid="project-name">{projectName}</h2>
+      ) : undefined}
+      {currentUser.uid ? (
+        <>
+          <ul className="tasks__list">
+            {tasks.map((task, index) => (
+              <li
+                key={`${task.id}`}
+                style={{ animationDelay: `${index * 40}ms` }}
+              >
+                <Checkbox id={task.id} taskDesc={task.task} />
+                <span>{task.task}</span>
+              </li>
+            ))}
+          </ul>
 
-      <ul className="tasks__list">
-        {tasks.map((task, index) => (
-          <li key={`${task.id}`} style={{ animationDelay: `${index * 40}ms` }}>
-            <Checkbox id={task.id} taskDesc={task.task} />
-            <span>{task.task}</span>
-          </li>
-        ))}
-      </ul>
+          <AddTask />
+        </>
+      ) : (
+          <div className="no-user-page">
+            <Checklist className="checklist-svg" />
 
-      <AddTask />
+            <button className="landing-page-cta">Log In</button>
+          </div>
+        )}
     </div>
   );
 };
