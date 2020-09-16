5a7a47ccc4e8f4bf1ee075512fa3b517d2bc5fe1
Florian Kulig <florian.kulig@web.de>
2020-09-16 15:55:33 +0200

    added method for generating a random color

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -33,3 +33,14 @@ export const generatePushId = (() => {
     return id;
   };
 })();
+
+export const generateRandomColor = () => {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return color;
+};
