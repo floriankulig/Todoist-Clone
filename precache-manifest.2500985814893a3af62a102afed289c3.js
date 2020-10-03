350d93aebfe7c951eb6d6babc799cec47d81aa73
Florian Kulig <florian.kulig@web.de>
2020-10-03 19:03:42 +0200

    Updates

---
+++ precache-manifest.2500985814893a3af62a102afed289c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb42dec712027d8ac437a3c4cf5b3f9",
+    "url": "/Todoist-Clone/index.html"
+  },
+  {
+    "revision": "8148e36b29750c1348ce",
+    "url": "/Todoist-Clone/static/css/main.1271fdfe.chunk.css"
+  },
+  {
+    "revision": "06c0e0ece7763ca43d81",
+    "url": "/Todoist-Clone/static/js/2.2e32b344.chunk.js"
+  },
+  {
+    "revision": "052157e0581e32a2b8fa8f7f1414d257",
+    "url": "/Todoist-Clone/static/js/2.2e32b344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8148e36b29750c1348ce",
+    "url": "/Todoist-Clone/static/js/main.9202892f.chunk.js"
+  },
+  {
+    "revision": "7ff498ce85db76051e48",
+    "url": "/Todoist-Clone/static/js/runtime-main.c79dc557.js"
+  },
+  {
+    "revision": "98d99ab058126e6133d0d9ac8ef88ec1",
+    "url": "/Todoist-Clone/static/media/checklist.98d99ab0.svg"
+  }
+]);
